ecedf64c727b50dc633370f2bae2027b20d053ca
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-17 00:45:40 -0500

    go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgetsapp/configs/menu/menu_item.dart';
+import 'package:widgetsapp/configs/router/router.dart';
 import 'package:widgetsapp/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -54,7 +56,10 @@ class _CustomListTile extends StatelessWidget {
         //     builder: (context) => const ButtonsScreen(),
         //   ),
         // );
-        Navigator.pushNamed(context, menuItem.link);
+
+        // Navigator.pushNamed(context, menuItem.link);
+
+        context.push(menuItem.link);
       },
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
     );
