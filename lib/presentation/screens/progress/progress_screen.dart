5e6ce37ce7a4afaf78ccb228ef7009004888cf3a
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-18 10:49:54 -0500

    Progress Indicators Finish

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -39,10 +39,11 @@ class _ProgressView extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          Text('Circular Indicator Controlado'),
+          Text('Circular y Linear Indicator Controlado'),
           SizedBox(
             height: 10,
-          )
+          ),
+          _ControlledProgressIndicator()
         ],
       ),
     );
@@ -52,7 +53,34 @@ class _ProgressView extends StatelessWidget {
 class _ControlledProgressIndicator extends StatelessWidget {
   const _ControlledProgressIndicator();
 
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((value) => value < 100),
+        builder: (context, snapshot) {
+          final progressValue = snapshot.data ?? 0.0;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 2,
+                  backgroundColor: Colors.black12,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: LinearProgressIndicator(
+                  value: progressValue,
+                ))
+              ],
+            ),
+          );
+        });
   }
 }
