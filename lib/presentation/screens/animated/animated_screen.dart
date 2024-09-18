589cddcf3f170dc86aa37ed97f0e9fa9b6f30af4
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-18 14:23:13 -0500

    Animando los cotainers...

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+  Duration duration = const Duration(seconds: 1);
+
+  void changeShape() {
+    final randon = Random();
+
+    width = randon.nextInt(300) + 120;
+    height = randon.nextInt(300) + 120;
+    color = Color.fromRGBO(
+        randon.nextInt(255), randon.nextInt(255), randon.nextInt(255), 1);
+    borderRadius = randon.nextInt(100).toDouble();
+    duration = Duration(milliseconds: randon.nextInt(3000) + 1000);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated Screen'),
+        title: const Text('Animated Container'),
       ),
       body: Center(
         child: AnimatedContainer(
-          duration: const Duration(seconds: 1),
-          curve: Curves.easeInCubic,
-          width: 200,
-          height: 130,
+          duration: duration,
+          curve: Curves.bounceInOut,
+          width: width < 0 ? 0 : width,
+          height: height,
           decoration: BoxDecoration(
-              color: Colors.indigo, borderRadius: BorderRadius.circular(20)),
+              color: color, borderRadius: BorderRadius.circular(borderRadius)),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {}, child: const Icon(Icons.play_arrow)),
+          onPressed: changeShape, child: const Icon(Icons.play_arrow)),
     );
   }
 }
