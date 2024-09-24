213d6227730c011ff0200c9f93ff9c41b56505fc
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-24 11:09:56 -0500

    Cambiar valores y modificadores

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgetsapp/presentation/providers/counter_provider.dart';
+import 'package:widgetsapp/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
@@ -9,14 +10,30 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final clickCounter = ref.watch(counterProvider);
+    final isDarkMode = ref.watch(isDarkModeProv);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(isDarkModeProv.notifier).update((state) => !state);
+              },
+              icon: isDarkMode
+                  ? const Icon(Icons.dark_mode)
+                  : const Icon(Icons.light_mode))
+        ],
       ),
       body: _CounterView(clickCounter: clickCounter),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          //ref.read(counterProvider.notifier).state++;
+          //Otra forma de cambiar el estado
+          ref.read(counterProvider.notifier).update(
+                (value) => value + 1,
+              );
+        },
       ),
     );
   }
